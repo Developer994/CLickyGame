0ff7ef76ae1785cf27b1bcc46a9bd84c7cd3685b
Silent Dev <rez_a@outlook.com>
2019-06-21 18:00:16 -0400

    Updates

---
+++ precache-manifest.0df37b386b823c9a31a3705b102d9ed7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d8fec97d8893622edce85a1f8d674e",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "ed6d5a4092f2e041d1c0",
+    "url": "/repository-name/static/css/main.481a79a7.chunk.css"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/repository-name/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "ed6d5a4092f2e041d1c0",
+    "url": "/repository-name/static/js/main.bde9c597.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/repository-name/static/media/bg.9a30e6c6.png"
+  },
+  {
+    "revision": "e3c05807a82f305a9174c0b223896bef",
+    "url": "/repository-name/static/media/knuckles1.e3c05807.png"
+  },
+  {
+    "revision": "1f9a7316678860e54e4b7c40df467584",
+    "url": "/repository-name/static/media/knuckles2.1f9a7316.png"
+  },
+  {
+    "revision": "ad576a719ec527890238b61c34dfc60a",
+    "url": "/repository-name/static/media/knuckles3.ad576a71.png"
+  },
+  {
+    "revision": "07ecadcbdc144c2da38220114b5a3a15",
+    "url": "/repository-name/static/media/sonic1.07ecadcb.jpg"
+  },
+  {
+    "revision": "3bbdb9758367e15fdaacf2a995f3fae3",
+    "url": "/repository-name/static/media/sonic2.3bbdb975.jpg"
+  },
+  {
+    "revision": "b19cabb7aeea6947d9669feeeac2cb25",
+    "url": "/repository-name/static/media/sonic3.b19cabb7.jpg"
+  },
+  {
+    "revision": "3f7f79d812eb0c9a80895d3c1a793dbe",
+    "url": "/repository-name/static/media/sonic4.3f7f79d8.jpg"
+  },
+  {
+    "revision": "6e9fe75f020c580e2c6aaef44b9fb376",
+    "url": "/repository-name/static/media/sonic5.6e9fe75f.png"
+  },
+  {
+    "revision": "d2a53cbcc23823d5da6bdc4ab83d1863",
+    "url": "/repository-name/static/media/tails1.d2a53cbc.png"
+  },
+  {
+    "revision": "a41006a9393b97b7922663e7a57713ae",
+    "url": "/repository-name/static/media/tails2.a41006a9.png"
+  },
+  {
+    "revision": "1b687562e3fad107efeb1aa7e4fd2218",
+    "url": "/repository-name/static/media/tails3.1b687562.png"
+  }
+]);
