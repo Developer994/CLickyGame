a1c9581c0aa88e8cfe89029f97276d8348fd2599
Silent Dev <rez_a@outlook.com>
2019-06-21 18:25:46 -0400

    Redeployed to github

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 var Navbar = props => (
     <div className="navbar ">
-        <div>Click Game</div>
+        <div>The Memory Game</div>
         <div className={props.navMsgColor}>{props.navMessage} </div>
         <div>
             Score: {props.score} <span className="pipe"></span> High Score: {props.highScore}
